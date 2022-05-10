fa880f63024cf5c1e53e76834208d6afb65b42f2
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-05-10 08:11:06 +0700

    first commit

---
+++ src/components/hello.components.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './hello.components.html',
+    styleUrls: ['./hello.components.css']
+})
+export class AppComponent {
+    title = 'my-app';
+}
