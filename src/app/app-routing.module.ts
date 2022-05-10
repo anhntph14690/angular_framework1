a4da2dc64fc4a64db4b3f387256a88d85e07af30
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-05-10 20:27:46 +0700

    B1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './components/hello/hello.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'hello', component: HelloComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
