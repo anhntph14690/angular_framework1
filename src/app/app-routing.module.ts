3ff51a26895a73e3a2cd581bbd7e6bc84a5c57a7
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-05-17 16:38:42 +0700

    feat: phân trang

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductsComponent } from './components/products/products.component';
 import { HelloComponent } from './components/hello/hello.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
-  { path: 'hello', component: HelloComponent },
-  {path: 'about', component: AboutComponent}
+  { path: 'home', component: HelloComponent },
+  { path: 'product', component: ProductsComponent },
+  { path: 'productAdd', component: ProductAddComponent },
+  { path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -7,6 +7,7 @@ import { IProduct } from '../../model/Product';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  
   @Input() products!: IProduct[]
   productDetail!: IProduct;
   constructor() { }
@@ -19,5 +20,25 @@ export class ProductsComponent implements OnInit {
   onHandleGetInfo(product: IProduct) {
     this.productDetail = product;
     console.log('product', product)
+  }
+}
+export class ListComponent {
+  productList: IProduct[] = [
+    {
+      id: 1,
+      name: "product A",
+      price: 200,
+      status: false
+    },
+    {
+      id: 2,
+      name: "product B",
+      price: 300,
+      status: true
+    }
+  ]
+  onHandleAdd(product: any) {
+    console.log("product", product);
+    this.productList.push(product);
   }
 }
